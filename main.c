6512572fd8716ffaac4b2b1ea24978b1032962b4
OrraymiosirisH <ohguevara.alvarez@gmail.com>
2016-11-14 23:52:05 -0800

    Update main.c

--- main.c
+++ main.c
@@ -1,9 +1,12 @@
+#include<stdio.h>
+#include<stdlib.h>
+
 int main()
 {
   int i;
-  for (i = 0; i < 10; i++)
+  for (i = 1; i < 33; i++)
   {
-    printf("Holberton\n");
+    printf("%d:Hello Holberton\n",i);
   }
   return 0;
 }
